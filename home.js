9047c6de54d7ffe4ac4753c12322e439ad08c67a
tylerklose <tylerklose@gmail.com>
2015-11-07 01:55:02 -0500

    clean up
    cleaned up some code that didn’t really work

--- home.js
+++ home.js
@@ -9,15 +9,10 @@ $(document).ready(function () {
 		$("#media-bar").css({ "height": media_bar_height });
 		$("#media-bar-wrapper > div > img").attr("height", media_bar_height);
 
-		var img_top = (window_height / 2) - ($("img").height() / 2);
-		$("#center-div-wrapper > img").css({ "top": (window_height / 2) - ($("img").height() / 2) - 25,
-		 																		 "left": (window_width / 2) - ($("img").width() / 2),
-																				  });
-		$("#links-div").css({ "top": $("img").height() + img_top - 75, "left": (window_width / 2) - ($("#links-div").width() / 2) });
-
-		$("#artwork").css({ "height": "100px", "width": "100px", "background-color": "white" });
-		$("<img id='media-bar-artwork' src='media/the-daily-dose-mix.jpg' width='" + media_bar_height + "' height='" + media_bar_height + "'>" );
-		$("#artwork").append($("#media-bar-artwork"));
-		//media_bar_artwork.css({ "height": "auto", "max-width": media_bar_height });
+		//attempt to position logo and text in center
+		var img_top = (window_height / 2) - ($("#logo").height() / 2);
+		//$("#logo").css({ "top": (window_height / 2) - ($("#logo").height() / 2),
+		// 																		 "left": (window_width / 2) - ($("#logo").width() / 2) });
+		$("#links-div").css({ "top": $("#logo").height() + img_top - 75, "left": (window_width / 2) - ($("#links-div").width() / 2) });
 
 });
